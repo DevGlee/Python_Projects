e4837aadaf12003443dd3c1799e5ad3b9193b743
Arnaud <arnaud.girondin@gmail.com>
2023-01-15 22:37:23 -0400

    update

---
+++ comments.py
@@ -0,0 +1,8 @@
+# Pour écrire les commentaires on utilisera la touche " # " a chaque debut de ligne 
+
+# Le print ci-dessous affichera le message "Bonjour tout le monde" 
+print("Bonjour tout le monde ")
+
+# Celui-ci ne s'affichera pas
+
+# print("print commenté")
---
+++ hello_world.py
@@ -0,0 +1,3 @@
+my_name = "devglee"
+
+print("Bonjour et Bienvenue" + " " +  my_name + " " + "!!!")
