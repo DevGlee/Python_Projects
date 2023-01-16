70117a4c5aa2d3ddbda5b4345908d6b6c0d2197a
Arnaud <arnaud.girondin@gmail.com>
2023-01-15 23:01:44 -0400

    Create variables_types.py

---
+++ variables_types.py
@@ -0,0 +1,36 @@
+#VARIABLES AND TYPES PYTHON
+
+##############################
+
+# NUMBERS
+
+mint1 = 15
+mint2 = 25
+print(mint1)
+
+mintresult= mint1 + mint2
+print(mintresult)
+
+mfloat = 852.639
+
+print(mfloat)
+
+#########################################"
+
+#STRINGS
+
+#################
+
+mstring1 = "Bonjour"
+print(mstring1)
+
+
+mstring2 = "Au revoir"
+
+print(mstring2)
+
+mstringresult= mstring1 + "  " +mstring2
+
+print(mstringresult)
+
+
