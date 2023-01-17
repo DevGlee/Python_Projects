a28f14435111805b18f9a0898d6b7ed03c41b87a
Arnaud <arnaud.girondin@gmail.com>
2023-01-17 19:00:59 -0400

    update

---
+++ operators.py
@@ -0,0 +1,45 @@
+##############             OPERATORS        #################
+
+numb = 5+13*2 / 2.0 #Ici on utilise l'operateur d'addition , de multiplication et de division
+print(numb)
+
+##########
+
+
+numb1 = 5%2   # Ici on utilise l'operateur modulo (reste d'une division)
+print(numb1)
+
+##########
+
+
+# Ici le double etoile est utilisé pour la puissance du nombre
+squared = 7 ** 2   # 7 puissance 2
+
+cubed = 2 ** 3       # 2 puissance 3
+
+
+print(squared)
+print(cubed)
+
+########    STRING OPERATORS #########
+
+##########
+
+# ici nous allons utiliser l'operateur de la concatenation pour afficher deux ou plusieurs chaines de caractèrres
+msg1 = "Bonjour" + " " + "tout le monde"
+print(msg1)
+
+gpmsg= "Salut" * 10   # l'etoile sert à afficher la chaine de caractère à repitition
+print(gpmsg)
+
+##########
+
+#Les operateurs peuvent être utilisé dans les listes
+even_nb= [2,4,6,8]
+odd_nb = [1,3,5,7]
+all_nb = odd_nb + even_nb
+print(all_nb)
+
+print([10,20,30]*3)
+
+
