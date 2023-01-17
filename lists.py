c40261d435edbf3aa5858c59c5fa12b395fb592f
Arnaud <arnaud.girondin@gmail.com>
2023-01-17 00:49:33 -0400

    update

---
+++ lists.py
@@ -0,0 +1,17 @@
+##############     LISTS    ################
+
+mlist = []
+
+mlist.append("Sangoku")
+mlist.append("Vegeta")
+mlist.append("Broly")
+
+#print((mlist[0])) #Affiche la première variable de la liste
+#print((mlist[1])) #""      "" deuxième   ""   ""   ""
+#print((mlist[2])) #""      "" troisième  ""   ""   ""
+
+# Afficher toutes les variables de la liste
+
+for x in mlist:    #nous allons parcourir tout la liste avec la variable x
+
+    print(x),
