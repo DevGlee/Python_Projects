0f4ca41326a7bb38d5dd9d79808e26a9032b62a7
Arnaud <arnaud.girondin@gmail.com>
2023-01-24 23:04:43 -0400

    Create loops.py

---
+++ loops.py
@@ -0,0 +1,39 @@
+##############  LOOPS #############
+
+
+primes = [2, 3, 5, 7]
+ 
+for prime in primes:
+   
+    print(prime)
+for x in range(5):
+   
+    print(x)
+        
+    
+for x in range(3,6):
+    
+    print(x)
+    
+for x in range(1,10,7):
+   
+    print(x)
+    
+    
+count = 0
+
+ #while count < 5:
+   # print(count)
+   # count += 1
+    
+
+while True:
+    print(count)
+    count += 1
+    if count >=5:
+        break
+    
+for x in range(10):
+    if x % 2 == 0:
+        continue
+    print(x)
